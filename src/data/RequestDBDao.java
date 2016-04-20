7eeeac3eeac9bbd95a44c22f05d574009bd25a8f
Angel M. Rivera Moreira <rivera.moreira@gmail.com>
2016-04-19 21:43:13 -0600

    Created the DAO and got the db to do a simple query.

--- src/data/RequestDBDao.java
+++ src/data/RequestDBDao.java
@@ -1,14 +1,34 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class RequestDBDao implements RequestDao {
 	private final String DRIVER_CLASS_NAME="com.mysql.jdbc.Driver";
 	private final String URL="jdbc:mysql://localhost:3306/companydb";
 	
 	@Override
 	public String executeQuery(String str) {
-		
-		
-		return null;
+		String input = str;
+		try{
+			Class.forName(DRIVER_CLASS_NAME);
+			
+			Connection conn = DriverManager.getConnection(URL, "student", "student" );
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(input);
+			while(rs.next()){
+				
+			}
+			
+			rs.close();
+			statement.close();
+			conn.close();	
+		}catch(Exception e){
+			System.err.println("The error was: " + e );
+		}
+		return input;
 	}
 	
 	
